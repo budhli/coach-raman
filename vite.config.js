5b523d513811d6fe291d8446ba6d0c175a7a7851
budhli <varshakapadia66@gmail.com>
2025-06-22 02:28:14 -0700

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-
+  plugins: [react()],
   build: {
     rollupOptions: {
       external: ['i18next', 'react-i18next']
     }
   }
-  plugins: [react()],
+  
 })
